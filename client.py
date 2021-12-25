20441141f2f3e077f92b0607a498974a284d13a8
Mega-batbat <95271461+mbatwc113114@users.noreply.github.com>
2021-12-25 05:30:48 +0000

    Initial commit

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+ip = "127.0.0.1"
+port = 9999
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# connection to server
+client.connect((ip,port))
+nick = input("enter nickname ")
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nick.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("connection stopped....")
+            client.close()
+            
+
+def write():
+    while True:
+        message = f"{nick} : {input('')}"
+        client.send(message.encode('ascii'))
+                    
+receive_thread = threading.Thread(target=receive)
+write_thread = threading.Thread(target=write)
+                    
+
+receive_thread.start()
+write_thread.start()
---
+++ server.py
@@ -0,0 +1,45 @@
+import socket
+import threading
+ip = "127.0.0.1"
+port = 9999
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((ip,port))
+clients = []
+nicknames = []
+
+server.listen()
+
+def aloud(message):
+    for client in clients:
+        client.send(message.encode("ascii"))
+        
+def handel(client):
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            aloud(message)
+            
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            aloud(f'{nickname} left')
+            break
+
+def receive_client():
+    while True:
+        
+        client, address = server.accept()
+        print("connected with {}".format(address))
+        client.send("NICK".encode("ascii"))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        aloud(f"{nickname} joined!")
+        handel_thread = threading.Thread(target=handel, args=(client,))
+        handel_thread.start()
+if __name__ == "__main__":
+    receive_client()
+        
