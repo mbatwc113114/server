0475cbf9ff630356254ae876344cef80d6ebf7bb
mbatwc113114 <95271461+mbatwc113114@users.noreply.github.com>
2021-12-25 11:06:24 +0530

    ip update

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
-ip = "127.0.0.1"
-port = 9999
+ip = "47.251.17.186"
+port = 55555
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((ip,port))
 clients = []
